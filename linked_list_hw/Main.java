25cec5cf797e6ac5f934db9a60dd4c0524c48a79
Ilya <steel.series.1337.el@gmail.com>
2023-06-30 12:09:15 +0300

    hw_done

---
+++ linked_list_hw/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.print();
+        list.reverse();
+        list.print();
+    }
+}
